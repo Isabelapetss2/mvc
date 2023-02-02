000c2ad1f6cf882c31b07701edf279a593f3f9b7
Thais Isabela Gonçalves <45473097+Isabelapetss2@users.noreply.github.com>
2023-02-01 22:08:35 -0300

    first commit

---
+++ app/controllers/ProdutoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;// define a pasta aonde fica todos os controllers
+use app\core\Controller;// utiliza o core aonde se encontra a classe mãe
+class ProdutoController extends Controller{
+    public function index(){
+       echo "<br>teste<br>";
+    }
+
+}
